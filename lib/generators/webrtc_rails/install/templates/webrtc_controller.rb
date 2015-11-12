da5cea310bba38a172a4af8866496d6bd36e1fb6
Koji Murata <koji.murata@dena.com>
2015-11-13 01:18:33 +0900

    bug fix

--- lib/generators/webrtc_rails/install/templates/webrtc_controller.rb
+++ lib/generators/webrtc_rails/install/templates/webrtc_controller.rb
@@ -1,6 +1,6 @@
 class WebrtcController < ApplicationController
   def send_message
-    WebsocketRails.users[params[:user_id]].send_message 'message', params[:message]
+    WebsocketRails.users[params[:user_id]].send_message 'webrtc', params[:message]
     render :json => 'OK'
   end
 end
