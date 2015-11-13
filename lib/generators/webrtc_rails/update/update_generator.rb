f18649fff6a146c1d958cf6c6976443116ab45eb
Koji Murata <koji.murata@dena.com>
2015-11-13 19:06:03 +0900

    rails generate webrtc_rails:update

--- lib/generators/webrtc_rails/update/update_generator.rb
+++ lib/generators/webrtc_rails/update/update_generator.rb
@@ -2,27 +2,16 @@ require 'rails'
 
 module WebrtcRails
   module Generators
-    class InstallGenerator < Rails::Generators::Base
+    class UpdateGenerator < Rails::Generators::Base
       source_root File.expand_path("../../templates", __FILE__)
 
-      def create_events_initializer_file
+      def update_all_files
         js_path = File.join('app', 'assets', 'javascripts')
         template 'main.js.coffee', File.join(js_path, 'webrtc_rails', 'main.js.coffee')
-        append_to_file File.join(js_path, 'application.js') do
-          out = ''
-          out << "\n\n// append by webrtc_rails\n"
-          out << "//= require webrtc_rails/main\n\n"
-        end
-      end
-
-      def create_webrtc_controller
         controller_path = File.join('app', 'controllers')
         template 'webrtc_controller.rb', File.join(controller_path, 'webrtc_controller.rb')
       end
 
-      def add_route
-        route "post '/webrtc', :to => 'webrtc#send_message'"
-      end
     end
   end
 end
